2a41219559f635d780e187fc3e4661b6e21012c8
Student <defounri@ualberta.ca>
2020-02-19 21:12:12 -0500

    all basic tests working, still some edge cases

--- assignment2.lisp
+++ assignment2.lisp
@@ -59,11 +59,16 @@
 	)
   )
 
+
+;binarizes an infix expression that already has division and multiplication binarized
+;uses recurive calls to group left to right.
 (defun binarize_add (E)
   (cond ((not (or (find '+ E) (find '- E))) E)
         (t (binarize_add (cons (list (car E) (cadr E) (caddr E)) (cdddr E)))))
   )
 
+;binarizes the multiplication and division of an infix expression - recursively calls itself left to right, ignoring any + or - operators and stopping when out of / and *
+;Both binarize_multi and binarize_add only worry about the current depth of list given (i.e. do not worry about deeper nested lists)
 (defun binarize_multi (E)
   (cond ((= (list-length E) 1 ) E)
 	((not (or (find '* E) (find '/ E))) E)
@@ -73,12 +78,14 @@
 	
   )
 
+;Helper function returns true if a list has further nested lists and nil if it is a flat list (one level)
 (defun isNested (E)
   (cond ((atom E) nil)
 	((atom (car E)) (isNested (cdr E)))
 	((atom (cdar E)) (isNested (cdr E)))
 	(t t)))
 
+;caller function used to binarize flat lists. if no multiplication or division operators are found, the bin_multi step is skipped to save time. Also deals with atomic values (i.e numbers)
 (defun binarize_after_nest (E)
   (cond ((atom E) E)
         ((or (position '* E) (position '/ E)) (car (binarize_add (binarize_multi E))))
@@ -86,63 +93,89 @@
 	(t nil)
 	))
 
+;function used to explore deeper levels of nesting and correctly group them back together.
+;Works similar to building a binary tree bottom-up, but requires a few more conditions to group binexpressions properly.
+;list-length 3 is a sort of base case, where list is in form (x op y). These can be joined together with a simple list function as there wont be more operators to add on later.
+;The other cases take the form (x op1 y op2 z ...) these are split into  (x) and (y op 2 z op3 ...).
+;because these ultimately should be in the same depth, append fuctions and either adding or removing depth (cons '() or car) is necessary.
 (defun explore_nesting (E)
   (cond ((numberp E) E)
 	((atom E) E)
+	((not (listp (cdr E))) E)
 	((not (isNested E)) (binarize_after_nest E))
-	((> (list-length (cddr E)) 1)   (binarize_after_nest (list
-					(car (explore_nesting (car E)))
-					(cadr E)
-					(car (explore_nesting (cddr E))))))
-	(t (binarize_after_nest (list
-				 (explore_nesting (car E))
-				 (cadr E)
-				 (caddr E))))
+	((= (list-length E) 3) (list
+			        (explore_nesting (car E))
+				(cadr E)
+				(binarize_after_nest (explore_nesting (caddr E)))))
+	
+	((and (isNested (car E)) (isNested (cddr E)))
+	 (binarize_after_nest (append
+			       (explore_nesting (car E))
+			       (cons (cadr E) '())
+			       (explore_nesting (cddr E)))))
+	
+	((isNested (car E))
+	 (binarize_after_nest (append
+			       (explore_nesting (car E))
+			       (cons (cadr E) '())
+			       (cons (explore_nesting (cddr E)) '()))))
+      	
+	((isNested (cddr E))
+	 (append (cons (explore_nesting (car E)) '())
+		 (cons (cadr E) '())
+		 (explore_nesting (cddr E))))
+	
+	(t (append
+	    (cons (explore_nesting (car E)) '())
+	    (cons (cadr E) '())
+	    (explore_nesting (cddr E))))
 	))
 
-(defun first_nest (E)
-  (cond ((numberp E) E)
-	((atom E) E)
-	((not (isNested E)) (binarize_after_nest E))
-	((> (list-length (cddr E)) 1)   (binarize_after_nest (list
-					(explore_nesting (car E))
-					(cadr E)
-					(explore_nesting (cddr E)))))
-	(t (binarize_after_nest (list
-				 (explore_nesting (car E))
-				 (cadr E)
-				 (caddr E))))
-	))
-
-;(defun binarize_nest (E)
-;   (cond ((atom E) E)
-;	 ((isNested E) (binarize_after_nest
-;			(cond
-;			 ((not (cddr E))
-;			  (cons (binarize_nest (car E)) '()))
-;			 ((isNested (binarize_nest (cddr E)))
-;			  (append
-;			   (list (binarize_nest (car E)) (cadr E))
-;			   (binarize_nest (cddr E))))
-;			 (t
-;			  (list
-;			   (binarize_nest (car E))
-;			   (cadr E)
-;			   (car (binarize_nest (cddr E)))))))
-;	  )
-;	 
-;       ((or (position '* E) (position '/ E)) (car (binarize_add (binarize_multi E))))
-;	((or (position '+ E) (position '- E)) (car (binarize_add E)))
-;	(t E)
-; ))
-
+;handles atomic values and gives an extra binarize call after the nesting is resolved for cases like ((1 + 2 + 3) / (4 + 5 + 6) * (7 + * 9)) to group the shallowest depth.
 (defun binarize (E)
   (cond ((atom E) E)
-	(t (binarize_after_nest (first_nest E)))
-	;((isNested E) (if (cddr E)
-	;		  (binarize_after_nest (list (binarize (car E)) (cadr E) (binarize (cddr E))))
-	;		  (binarize_after_nest (binarize (car E)))))
-        ;((or (position '* E) (position '/ E)) (car (binarize_add (binarize_multi E))))
-	;((or (position '+ E) (position '- E)) (car (binarize_add E)))
-	;(t (binarize_after_nest (binarize_nest E)))
+	((atom (cdr E)) E)
+	(t (binarize_after_nest (explore_nesting E)))
   ))
+
+(defun simplify (E)
+  (simplifybinary (binarize E)))
+
+
+(defun sub_var_flat (Bindings E)
+    (cond ((not Bindings) E)
+	((atom E) (if (eql (caar Bindings) E)
+		      (cadar Bindings)
+		      (sub_var_flat (cdr Bindings) E)))
+	((not (find (caar Bindings) E))
+	 (sub_var_flat (cdr Bindings) E))
+	((position (caar Bindings) E)
+	  (setf
+	   (nth
+	    (position (caar Bindings) E)
+	    E)
+	   (simplify (cadar Bindings)))
+	  (sub_var_flat Bindings E))
+	)
+)
+
+(defun substitutevar (Bindings E)
+  (cond 
+	((not (isNested E)) (sub_var_flat Bindings E))
+	 
+	((and (isNested (car E)) (isNested (cddr E)))
+	 (append (cons (substitutevar Bindings (car E)) '()) (cons (cadr E) '()) (cons (substitutevar Bindings (cddr E)) '())))
+	((and (isNested (car E)) (cddr E))
+	 (append (cons (substitutevar Bindings (car E)) '()) (cons (cadr E) '()) (cons (sub_var_flat Bindings (cddr E)) '())))
+	((isNested (car E))
+	 (substitutevar Bindings (car E)))
+	((isNested (cddr E))
+	 (list (sub_var_flat Bindings (car E)) (cadr E) (substitutevar Bindings (cddr E))))
+	((cddr E) (append (cons (sub_var_flat Bindings (car E)) '()) (cons (cadr E) '()) (substitutevar Bindings (cddr E))))
+	(t (sub_var_flat Bindings (car E)))
+	
+	))
+
+(defun simplifyvar (Bindings E)
+  (simplify (substitutevar Bindings E)))
+       
